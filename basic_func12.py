7fdb1ad248147f48eea8f70b2335a8be92203252
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-06-20 00:40:16 +0500

    basic_func12

--- basic_func12.py
+++ basic_func12.py
@@ -1,9 +1,8 @@
 # Create a function called main.
 # Create function argument a.
-from math import floor
 def main(a):
 # Return the value of a.
-    a = floor(a)
+    a = round(a,2)
     return a
 # Round the result to 2 decimal places.
-print(main(12.36))
+print(main(12.36255))
