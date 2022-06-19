dbde5c2f7966f0336d95706815f25265e5a20ace
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-06-20 01:38:32 +0500

    basic_func19

--- basic_func19.py
+++ basic_func19.py
@@ -2,7 +2,7 @@
 # Create function arguments a and b.
 def main(a,b):
 # Return the absolute value of the difference between a and b.
-    x = (a+b)-(a-b) 
+    x = a-b
     return x
 print(main(25,21))
 
