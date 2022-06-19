4a83b1dba4c1af8797450f9ba53b0e0c6cd8a36e
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-06-19 23:44:56 +0500

    basic_func12

--- basic_func12.py
+++ basic_func12.py
@@ -3,7 +3,7 @@
 from math import ceil
 def main(a):
 # Return the value of a.
-    x = ceil(a)
-    return x
+    a = ceil(a)
+    return a
 # Round the result to 2 decimal places.
 print(main(12.369854))
