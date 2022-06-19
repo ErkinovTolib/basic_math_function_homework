44cabfc7e15b949f191554dc61921a6f155dd562
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-06-20 01:19:31 +0500

    basic_func18

--- basic_func18.py
+++ basic_func18.py
@@ -1,8 +1,6 @@
 # Create a function called main.
 # Create function arguments a.
-from cmath import pi
-
-
+from math import pi
 def main(a):
 # Assign the value pi to the variable "a" and return.
     a = pi
