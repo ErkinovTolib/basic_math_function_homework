17f9317885f76481c0985a046003ffce28dc037a
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-06-20 01:14:04 +0500

    basic_func17

--- basic_func17.py
+++ basic_func17.py
@@ -1,3 +1,8 @@
 # Create a function called main.
 # Create function arguments a.
+from math import pi
+def main(a):
 # Assign the value pi to the variable "a" and return.
+    a = pi
+    return a
+print(main(3.1415))
