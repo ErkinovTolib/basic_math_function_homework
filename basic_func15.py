4ce8d4b51848e99f2368408988edb32563e37e59
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-06-20 00:58:42 +0500

    basic_func15

--- basic_func15.py
+++ basic_func15.py
@@ -1,3 +1,9 @@
 # Create a function called main.
 # Create function arguments a and b.
+from math import abs
+def main(a,b):
 # Return the modulus of a and b.
+    x = abs(a)
+    y = abs(b)
+    return x,y
+print(main(27,8))
