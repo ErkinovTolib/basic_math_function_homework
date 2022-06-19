9e6a4984e1a09fbcdf1f334bbae31a7b347e956d
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-06-19 23:27:57 +0500

    basic_func12

--- basic_func12.py
+++ basic_func12.py
@@ -1,4 +1,10 @@
 # Create a function called main.
 # Create function argument a.
+from math import ceil, floor
+
+
+def main(a):
 # Return the value of a.
+    return floor(a)
 # Round the result to 2 decimal places.
+print(main(12.369854))
